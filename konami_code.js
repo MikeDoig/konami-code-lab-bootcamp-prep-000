1be6355b6f72570a4af18989cbff78fc4778ecbe
Mike Doig <mike.j.doig@gmail.com>
2019-02-18 18:14:14 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -11,34 +11,17 @@ const codes = [
   "a"
 ];
 
-// function init() {
-//   let index = 0;
-//   document.body.addEventListener('keydown', (e) => {
-//     const key = e.key;
-//     index = (codes[index] == key) ? ++index: 0;
-//     if (index==codes.length) {
-//       alert('You did it!');
-//     index = 0;
-    
-//   }
-//   });
-// }
-
 function init() {
   let index = 0;
   
   document.body.addEventListener('keydown', function(e)  {
     const key = e.key;
     if (codes[index] == key) {
-    // index = key;
     index++;
     }
     if (index == codes.length) {
       alert('You did it! Secret found!');
       index = 0;
     }
-    // else {
-    //   index = 0;
-    // }
   });
 }
