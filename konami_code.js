e4d810a84c3faddf1f8917ed8d381ee3fe9a1231
Mike Doig <mike.j.doig@gmail.com>
2019-02-18 18:04:53 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -29,6 +29,10 @@ function init() {
   
 //   document.body.addEventListener('keydown', function(e)  {
 //     const key = e.key;
+//     if (key == codes[index]) {
+//     index = key
+//     index++
+}
 //     if (index = (key === codes[index])) {
 //       i++;
 //       if (index == codes.length) {
